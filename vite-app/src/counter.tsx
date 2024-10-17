a83071b43b26cc2ddeff229abffc8e107190dd85
liukaixin <15713857919@163.com>
2024-10-17 18:19:40 +0800

    test vite

---
+++ vite-app/src/counter.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import "./index.less";
+
+const Counter: React.FC = () => {
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => {
+    setCount((e) => {
+      return e + 1;
+    });
+  };
+
+  useEffect(() => {
+    console.log("effect...start2", count);
+    return () => {
+      console.log("effect...end", count);
+    };
+  }, [count]);
+
+  return (
+    <div>
+      <button onClick={handleIncrement}>{count}</button>
+      <p>
+        <span>aaaa121129</span>
+      </p>
+      <a
+        href="https://activity.lagou.com/app/business/service-agreement.html"
+        target="_blank"
+        className="transform"
+      >
+        《拉勾网在线增值服务协议》
+      </a>
+    </div>
+  );
+};
+
+export default Counter;
